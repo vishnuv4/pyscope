163dfb5837816033545253b618521ef12309f9b6
Vishnu Venkatesh <vishnuv@seas.upenn.edu>
2024-06-29 21:04:33 -0700

    added analog scope

--- scripts/analog_scope.py
+++ scripts/analog_scope.py
@@ -1,19 +1,12 @@
-import json
 import matplotlib.pyplot as plt
-import argparse
+import numpy as np
 import collections
 
-parser = argparse.ArgumentParser(description='Signals')
-parser.add_argument('file', type=str, help="Name of the signal file")
+signals = {}
 
-args = parser.parse_args()
-filename = args.file
-
-try:
-    with open(filename, "r", encoding="utf-8") as file:
-        signals = json.load(file)
-except FileNotFoundError:
-    raise RuntimeError(f"No file called {filename}.json")
+signals["ch0"] = np.sin(np.linspace(0, 2 * np.pi, 80))
+signals["ch1"] = np.cos(np.linspace(0, 2 * np.pi, 80))
+signals["ch2"] = np.sin(np.linspace(0, 2 * np.pi, 80))
 
 if len(signals) > 4:
     raise RuntimeError("Can't support more than 4 channels")
@@ -22,32 +15,36 @@ channel_list = ["ch0", "ch1", "ch2", "ch3"]
 if not all(key in channel_list for key in signals.keys()):
     raise RuntimeError("Invalid channel name")
 
-signals = collections.OrderedDict(sorted(signals.items()))
-
-i = 0
-for ch in signals:
-    signals[ch] = [int(bit) for bit in "".join(signals[ch].split())]
-    signals[ch] = [((-2 * i) + k) for k in signals[ch]]
-    i += 1
-
 lengths = [len(lst) for lst in signals.values()]
 if not all(length is lengths[0] for length in lengths):
     raise ValueError("Signal lengths are not the same")
 
+signals = collections.OrderedDict(sorted(signals.items()))
+
 t = range(lengths[0])
+i=0
+for ch in signals:
+    signals[ch] = [((-2 * i) + k) for k in signals[ch]]
+    i += 1
 
 # Each array element corresponds to the value immediately after the grid line
 
 colors = ['xkcd:yellow', 'xkcd:cyan', 'xkcd:neon purple', 'xkcd:bright blue']
 plt.figure(figsize=(15, 6))
-i = 0
+i = k = 0
 for ch in signals:
-    plt.step(t, signals[ch], where='pre', color=colors[i])
+    print(ch)
+    print(signals[ch])
+    print('\n')
+    plt.plot(t, signals[ch], color=colors[k])
     i = i + 1
+    k = i % 4
 
 plt.gca().set_facecolor('black')
 plt.xlabel('')
 plt.ylabel('')
+print(list(signals.keys())[0])
+print(list(signals.keys())[-1])
 plt.ylim(min(signals[list(signals.keys())[-1]]) - 1, max(signals[list(signals.keys())[0]]) + 1)
 
 plt.xticks(ticks=t, labels='')
