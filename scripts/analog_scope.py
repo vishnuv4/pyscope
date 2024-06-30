116e5fc0793d23c36f90d273d85e43fd76d500c3
Vishnu Venkatesh <vishnuv@seas.upenn.edu>
2024-06-29 21:30:27 -0700

    bugfix

--- scripts/analog_scope.py
+++ scripts/analog_scope.py
@@ -9,9 +9,12 @@ signals = {}
 Modify the signals here
 '''
 
+arr1 = np.sin(np.linspace(0, 2 * np.pi, 40))
+arr2 = 1 + np.cos(np.linspace(0, 2 * np.pi, 40))
+
 signals["ch0"] = np.sin(np.linspace(0, 2 * np.pi, 80))
-signals["ch1"] = np.cos(np.linspace(0, 2 * np.pi, 80))
-signals["ch2"] = np.sin(np.linspace(0, 2 * np.pi, 40))
+signals["ch1"] = 2 * np.cos(np.linspace(0, 2 * np.pi, 80))
+signals["ch2"] = np.concatenate([arr1, arr2])
 
 ######################################################################
 
@@ -28,21 +31,23 @@ if not all(length is lengths[0] for length in lengths):
 
 signals = collections.OrderedDict(sorted(signals.items()))
 
+displacement = 2
+keys = list(signals.keys())
+for i in range(len(keys)-1):
+    displacement = abs(min(signals[keys[i]])) + abs(max(signals[keys[i+1]])) + 2
+
 t = range(lengths[0])
 i=0
 for ch in signals:
-    signals[ch] = [((-2 * i) + k) for k in signals[ch]]
+    signals[ch] = [((-displacement * i) + k) for k in signals[ch]]
     i += 1
 
 # Each array element corresponds to the value immediately after the grid line
 
 colors = ['xkcd:yellow', 'xkcd:cyan', 'xkcd:neon purple', 'xkcd:bright blue']
-plt.figure(figsize=(15, 6))
+plt.figure(figsize=(16.18, 10))
 i = k = 0
 for ch in signals:
-    print(ch)
-    print(signals[ch])
-    print('\n')
     plt.plot(t, signals[ch], color=colors[k])
     i = i + 1
     k = i % 4
@@ -50,12 +55,10 @@ for ch in signals:
 plt.gca().set_facecolor('black')
 plt.xlabel('')
 plt.ylabel('')
-print(list(signals.keys())[0])
-print(list(signals.keys())[-1])
 plt.ylim(min(signals[list(signals.keys())[-1]]) - 2, max(signals[list(signals.keys())[0]]) + 2)
 
 plt.xticks(ticks=t, labels='')
-plt.yticks(ticks=[(-2 * i) for i in range(len(lengths))], labels=[channel for channel in signals.keys()])
+plt.yticks(ticks=[(-displacement * i) for i in range(len(lengths))], labels=[channel for channel in signals.keys()])
 
 plt.grid(True)
 
